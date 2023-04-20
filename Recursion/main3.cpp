b03234a06a50ee1e0c1461e56786b47f85ef5222
ram kumar <rammaniyari@gmail.com>
2023-04-20 23:26:38 +0530

    nth-term-of-fibonacci

---
+++ Recursion/main3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n)
+{
+
+    if (n == 1)
+        return 0;
+    if (n == 2)
+        return 1;
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+
+    int n;
+    cout << "Enter the term you want to see : " << endl;
+    cin >> n;
+
+    int ans = fib(n);
+
+    cout << n << "th term is " << ans << endl;
+
+    return 0;
+}
