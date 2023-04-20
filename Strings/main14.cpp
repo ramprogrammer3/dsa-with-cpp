e3994b42f13f57fdf0f25a577c56a13a31d734fb
ram kumar <rammaniyari@gmail.com>
2023-04-20 11:15:27 +0530

    valid-anagram

--- Strings/main14.cpp
+++ Strings/main14.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+bool isAnagram(string a, string b)
+{
 
+    int freqTable[256] = {0};
+    for (int i = 0; i < a.size(); i++)
+    {
+        freqTable[a[i]]++;
+    }
+
+    for (int i = 0; i < b.size(); i++)
+    {
+        freqTable[b[i]]--;
+    }
+
+    for (int i = 0; i < 256; i++)
+    {
+        if (freqTable[i] != 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+
+    string s1 = "ramkumar";
+
+    string s2 = "marmarku";
+
+    if (isAnagram(s1, s2))
+    {
+        cout << "your strings are anagram " << endl;
+    }
+    else
+    {
+        cout << "Your strings are not anagram " << endl;
+    }
 
     return 0;
 }
