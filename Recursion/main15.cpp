0bb0158e3e9df8fe6b82f7e45d465626a5478324
ram kumar <rammaniyari@gmail.com>
2023-04-24 12:42:14 +0530

    maximum-sum-of-non-adjacent-element

---
+++ Recursion/main15.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+using namespace std;
+
+void solve(vector<int> &arr, int i, int sum, int &maxi)
+{
+
+    if (i >= arr.size())
+    {
+        maxi = max(sum, maxi);
+        return;
+    }
+
+    solve(arr, i + 2, sum + arr[i], maxi);
+    solve(arr, i + 1, sum, maxi);
+}
+
+int main()
+{
+
+    vector<int> arr{2, 1, 4, 9};
+    int sum = 0;
+    int i = 0, maxi = INT_MIN;
+
+    solve(arr, i, sum, maxi);
+
+    cout << maxi << endl;
+    return 0;
+}
