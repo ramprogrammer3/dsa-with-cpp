3433114a973b672dd1db847af8a938d0125ca5f6
ram kumar <rammaniyari@gmail.com>
2023-05-08 09:38:32 +0530

    inheritance

--- OOPs/main2.cpp
+++ OOPs/main2.cpp
@@ -7,10 +7,14 @@ class Animal{
     int age;
     int weight;
 
+    void eat(){
+        cout << "Eating " << endl;
+    }
+
 };
 
 
-class Dog:private Animal{
+class Dog:public Animal{
 
     public:
     void print(){
@@ -24,6 +28,7 @@ int main(){
     Dog d1;
     // cout << d1.age << endl;
     d1.print();
+    d1.eat();
 
     return 0;
 }
