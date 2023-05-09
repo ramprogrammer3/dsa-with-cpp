2ebed16a5ef3456eec959445f0fa4cc47aad7a39
ram kumar <rammaniyari@gmail.com>
2023-05-09 11:50:01 +0530

    static-keyword

---
+++ OOPs/main13.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class abc
+{
+
+public:
+    static int x, y;
+
+    static void print()
+    {
+        cout << x << " " << y << endl;
+    }
+};
+
+int abc::x;
+int abc::y;
+
+int main()
+{
+
+    abc obj1;
+    obj1.x = 1;
+    obj1.y = 2;
+    obj1.print();
+    abc obj2;
+    obj2.x = 10;
+    obj2.y = 20;
+    obj1.print();
+    obj2.print();
+
+    return 0;
+}
