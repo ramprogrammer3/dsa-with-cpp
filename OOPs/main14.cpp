70fa891c3d92afb1ab5f43b790c6c1105b91f277
ram kumar <rammaniyari@gmail.com>
2023-05-09 12:39:21 +0530

    inline-function

---
+++ OOPs/main14.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+inline void numberShow(int num){
+    cout << num << endl;
+}
+
+int main()
+{
+
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+    numberShow(10);
+
+    return 0;
+}
