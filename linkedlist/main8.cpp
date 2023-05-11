68513db8e2e0986c09d62dc867af554a8dc992b8
ram kumar <rammaniyari@gmail.com>
2023-05-11 14:14:07 +0530

    insert-at-head-in-doubly-linkedlist

---
+++ linkedlist/main8.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+
+public:
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void insertAtHead(Node *&head, Node *&tail, int data)
+{
+    if (head == NULL)
+    {
+        Node *newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    Node *newNode = new Node(data);
+    newNode->next = head;
+    head->prev = newNode;
+    head = newNode;
+}
+
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+
+    Node *first = new Node(10);
+    Node *second = new Node(20);
+    Node *third = new Node(30);
+
+    first->next = second;
+    second->prev = first;
+
+    second->next = third;
+    third->prev = second;
+
+    Node *head = first;
+    Node *tail = third;
+
+    cout << "printing linked list " << endl;
+    print(head);
+    cout << endl;
+    insertAtHead(head, tail, 333);
+    print(head);
+
+    return 0;
+}
