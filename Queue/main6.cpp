3d4e838bacdb8ef0baa57126eac787995539f62d
ram kumar <rammaniyari@gmail.com>
2023-05-17 15:18:46 +0530

    reverse-a-queue

---
+++ Queue/main6.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+using namespace std;
+
+void print(queue<int> q)
+{
+
+    while (!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+
+    cout << endl;
+}
+
+void reverse(queue<int> &q)
+{
+
+    stack<int> s;
+
+    while (!q.empty())
+    {
+        s.push(q.front());
+        q.pop();
+    }
+
+    while (!s.empty())
+    {
+        q.push(s.top());
+        s.pop();
+    }
+}
+
+int main()
+{
+
+    queue<int> q;
+    q.push(3);
+    q.push(6);
+    q.push(9);
+    q.push(2);
+    q.push(8);
+    q.push(5);
+
+    reverse(q);
+
+    print(q);
+
+    return 0;
+}
