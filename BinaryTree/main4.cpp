b54009916dc573f96d5bc2e5d6f0dfc4d5516f43
ram kumar <rammaniyari@gmail.com>
2023-05-19 12:49:30 +0530

    level-order-traversal

---
+++ BinaryTree/main4.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+
+};
+
+Node* buildTree(){
+    int data;
+    cout << "Enter the data " << endl;
+    cin >> data;
+
+    if(data == -1){
+        return NULL;
+    }
+
+    Node * root = new Node(data);
+    cout << "Enter the data for left part : " <<data << " node " << endl;
+    root->left = buildTree();
+    cout << "Enter the data for right par : " << data << " node " << endl;
+    root->right = buildTree();
+    return root;
+
+}
+
+int main(){
+
+
+
+    return 0;
+}
