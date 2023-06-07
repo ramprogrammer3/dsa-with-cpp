6a2a22f99aa6077ab697ad0a7fedfadb4d19b784
ram kumar <rammaniyari@gmail.com>
2023-06-07 07:08:23 +0530

    last-occurence-in-sorted-array

---
+++ Arrays/main20.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int lastOccurence(int *arr, int n, int element)
+{
+
+    int s = 0;
+    int e = n - 1;
+
+    int m = (s + e) / 2;
+
+    int ans = -1;
+
+    while (s <= e)
+    {
+
+        if (arr[m] == element)
+        {
+            ans = m;
+            s = m + 1;
+        }
+
+        else if (arr[m] < element)
+            s = m + 1;
+        else
+            e = m - 1;
+
+        m = (s + e) / 2;
+    }
+
+    return ans;
+}
+
+int main()
+{
+
+    int arr[] = {1, 2, 4, 4, 4, 4, 4, 4, 8, 9, 11, 15, 19, 23};
+
+    int n = 14;
+    int element = 4;
+
+    int searchElement = lastOccurence(arr, n, element);
+
+    cout << "Element found at index " << searchElement << endl;
+    cout << "element is " << arr[searchElement] << endl;
+
+    return 0;
+}
