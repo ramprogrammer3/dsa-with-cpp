4548244fc8160688c10fd8c08bd7f2a95c42c4a7
ram kumar <rammaniyari@gmail.com>
2023-06-23 20:32:11 +0530

    printing-frequency-of-character-using-map

---
+++ hashmapandtrie/main2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int main(){
+
+
+    string str = "thiruvananthapuram";
+    unordered_map<char , int> freq;
+
+    for(int i = 0; i<str.length(); i++){
+        char ch = str[i];
+        freq[ch]++;
+    }
+
+    cout << "printng map " << endl << endl;
+
+    for(auto i : freq){
+        cout << i.first << " -> " << i.second << endl;
+    }
+
+    return 0;
+}
